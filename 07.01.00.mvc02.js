2a6fce3bb6ea1c8fb731df269a03d5366a7db358
p3kj3t <66711587+p3kj3t@users.noreply.github.com>
2020-07-03 22:08:37 +0700

    Add files via upload

--- 07.01.00.mvc02.js
+++ 07.01.00.mvc02.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 2108
+//const port = process.env.PORT|| 3000
 var session = require('express-session')
 var path = require('path');
 var router = express.Router();  
@@ -29,6 +30,7 @@ app.set('view engine', 'ejs');              ///***** */
 app.use(session(sess));
 app.listen(process.env.PORT|| 3000);
 
+
 /// ***************** ***************** *****************
 /// ***************** ***************** Config DB CONNECTION
 const MongoClient = require('mongodb').MongoClient;
@@ -37,19 +39,22 @@ const mongosee = require('mongoose');
 /// *****************  Models
 const Product = require('./models/product');
 const Staff = require('./models/staff');
+const Order = require('./models/order').order;
+const orderSchema  = require('./models/order').schema;
 
 
 /// *****************  Controllers
 const viewLogin = require('./controllers/logincontroller');
 
 /// ***************** 
-//const urilc = 'mongodb://localhost:8080/ShopMoi';
-const uri = "mongodb+srv://db02:YgAWa0nvjXWYdwHj@cluster0-uqxs1.mongodb.net/ShopMoi?retryWrites=true&w=majority";
-
+//const uri = 'mongodb://localhost:27017/ATN_Shop';
+//const urirem = "mongodb+srv://db03:aGBZRta11CBmt8qL@cluster0-q8a6f.mongodb.net/ATN_Shop?retryWrites=true&w=majority";
+const uri = "mongodb+srv://minhhhieumh14:hieu123@cluster0.xssol.mongodb.net/ATN_Company?retryWrites=true&w=majority";
 
 /// ***************** ***************** *****************
 /// ***************** Database & Bảng dữ liệu cần Truy vấn
-const NameDataBase =  "atnshop"; // "CloudDB";
+//const NameDataBase =  "ATN_Shop"; // "CloudDB";
+const NameDataBase =  "ATN_Company";
 var xflag = 0;
 var vResult = [];
 var accLogin = null;
@@ -97,7 +102,7 @@ async function responseDB(response, xview, xModel, xQuery, xparams, xtag, xNext=
 
         if (kq) {
             xparams[xtag] = kq;            
-            console.log(xview + "\t THanh cong !");
+            console.log(xview + "\t Thanh cong !");
             response.render(xview, xparams);
         } else {
             response.render(xNext, { mesg : "... KO co Data DB ! "} );
@@ -126,10 +131,10 @@ app.get('/login', viewLogin);
 /// ***************** ***************** *****************
 app.get('/products', viewProducts);
 async function viewProducts(request, response) {
-    if (typeof (request.session.login_user) == "undefined")
-    {
-        response.redirect("/login");
-    }
+    //if (typeof (request.session.login_user) == "undefined")
+    //{
+        //response.redirect("/login");
+    //}
     responseDB(response, "productlist",
 				Product, {}, { username : request.session.login_user }, "productlist");
 }
@@ -173,18 +178,34 @@ function viewOrder(request, response) {
 /// /payment
 /// ***************** ***************** *****************
 app.get('/payment', viewPayment);
-function viewPayment(request, response) {
+async function viewPayment(request, response) {
     //response.send("Web - PAYMENT page !" + request.query.dssp);
     var dssp = request.query.dssp;
     var listkq = dssp.split("_");
+    var vusername = request.session.login_user;
 
     listsp = [];
-    for (i=0; i< listkq.length / 2; i++) {
+    for (i=0; i< listkq.length / 3; i++) {
         listsp.push(
-            { Name : "Tivi " + listkq[i*2], Price : 30000, Num: listkq[i*2+1]},
+            { Name : "Tivi " + listkq[i*3], Price : listkq[i*3+2], Num: listkq[i*3+1]},
         );
     }
-    
+
+    var orderX = new Order( {
+        _id : new mongosee.mongo.ObjectId(),
+        StaffID : vusername,
+        ItemsList : dssp,
+        Total : 0
+    } );
+
+    var connect = mongosee.createConnection(uri);
+    const xOrder = mongosee.model("Order", orderSchema, "Order");
+    await xOrder.create({
+        _id : new mongosee.mongo.ObjectId(),
+        StaffID : vusername,
+        ItemsList : dssp,
+        Total : 0
+    });
 
     response.render("payment", { username : request.session.login_user , productlist : listsp });
 }
@@ -228,3 +249,4 @@ function viewReview(request, response) {
 /// ***************** ***************** *****************
 /// ***************** ***************** *****************
 app.listen(port, () => console.log(`\n\tWeb app listening at http://localhost:${port}`));
+© 2020 GitHub, Inc.
